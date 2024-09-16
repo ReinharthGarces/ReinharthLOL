71037eaaf5baefd98675add5cfa93b6f227b2ee5
Reinharth Garces <rgarces@compragamer.net>
2024-09-16 17:33:39 -0300

    Runes working

--- src/app/features/runes/runes/runes.component.ts
+++ src/app/features/runes/runes/runes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RunesService } from '../../../core/runes.service';
-
 @Component({
   selector: 'app-runes',
   templateUrl: './runes.component.html',
@@ -8,14 +7,15 @@ import { RunesService } from '../../../core/runes.service';
 })
 
 export class RunesComponent implements OnInit {
-
   runes: any[] = [];
 
   constructor(private runesService: RunesService) { }
 
   ngOnInit(): void {
     this.runesService.getRunes().subscribe(data => {
-      this.runes = data;
+      // Aquí asumimos que las primeras 5 runas son las principales
+      // Cada una tendrá un array `subRunes` que contiene las runas relacionadas
+      this.runes = data.slice(0, 5);  // Las 5 runas principales
     });
   }
 }
