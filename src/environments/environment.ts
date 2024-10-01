7bc532c50305ce19812a02496d61426faba8d03a
Reinharth Garces <rjgarces7@gmail.com>
2024-09-30 21:09:30 -0300

    proyect deploy 1.6

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  envName: 'Desarrollo local',
+  apiURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/data/es_ES/champion',
+  imgURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/img',
+  champImgURL: 'https://ddragon.leagueoflegends.com/cdn/img/champion/loading',
+  runesURL: 'https://ddragon.leagueoflegends.com/cdn/14.18.1/data/es_ES/runesReforged.json',
+  runesImgURL: 'https://ddragon.leagueoflegends.com/cdn/img',
+  spellsURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/data/es_ES/summoner.json',
+  spellsImgURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/img/spell',
+  itemsURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/data/es_ES/item.json',
+  itemsImgURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/img/item'
+};
--- src/environments/environmet.development.ts
+++ src/environments/environmet.development.ts
@@ -1,12 +1,12 @@
 export const environment = {
   envName: 'Desarrollo local',
-  apiURL: 'http://ddragon.leagueoflegends.com/cdn/14.19.1/data/es_ES/champion',
-  imgURL: 'http://ddragon.leagueoflegends.com/cdn/14.19.1/img',
+  apiURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/data/es_ES/champion',
+  imgURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/img',
   champImgURL: 'https://ddragon.leagueoflegends.com/cdn/img/champion/loading',
   runesURL: 'https://ddragon.leagueoflegends.com/cdn/14.18.1/data/es_ES/runesReforged.json',
-  runesImgURL: 'http://ddragon.leagueoflegends.com/cdn/img',
+  runesImgURL: 'https://ddragon.leagueoflegends.com/cdn/img',
   spellsURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/data/es_ES/summoner.json',
   spellsImgURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/img/spell',
-  itemsURL: 'http://ddragon.leagueoflegends.com/cdn/14.19.1/data/es_ES/item.json',
-  itemsImgURL: 'http://ddragon.leagueoflegends.com/cdn/14.19.1/img/item'
+  itemsURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/data/es_ES/item.json',
+  itemsImgURL: 'https://ddragon.leagueoflegends.com/cdn/14.19.1/img/item'
 };
